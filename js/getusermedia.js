976a937bbaf00f8ece5930444191c074eac750f2
ChathuraSam <chathura.samarajeewa@gmail.com>
2018-11-28 01:27:05 +0530

    added users.html and copied index to it

--- js/getusermedia.js
+++ js/getusermedia.js
@@ -58,29 +58,29 @@ console.log("button triggered");
 //getting capture
 
 (function() {
-    "use strict";
- 
-    var video, $output;
-    var scale = 0.25;
- 
-    var initialize = function() {
-        $output = $("#output");
-        video = $("#video").get(0);
-        $("#capture").click(captureImage);                
-    };
- 
-    var captureImage = function() {
-        var canvas = document.createElement("canvas");
-        canvas.width = video.videoWidth * scale;
-        canvas.height = video.videoHeight * scale;
-        canvas.getContext('2d')
-              .drawImage(video, 0, 0, canvas.width, canvas.height);
- 
-        var img = document.createElement("img");
-        img.src = canvas.toDataURL();
-        $output.prepend(img);
-    };
- 
-    $(initialize);            
- 
+	"use strict";
+
+	var video, $output;
+	var scale = 0.25;
+
+	var initialize = function() {
+		$output = $("#output");
+		video = $("#video").get(0);
+		$("#capture").click(captureImage);                
+	};
+
+	var captureImage = function() {
+		var canvas = document.createElement("canvas");
+		canvas.width = video.videoWidth * scale;
+		canvas.height = video.videoHeight * scale;
+		canvas.getContext('2d')
+		.drawImage(video, 0, 0, canvas.width, canvas.height);
+
+		var img = document.createElement("img");
+		img.src = canvas.toDataURL();
+		$output.prepend(img);
+	};
+
+	$(initialize);            
+
 }());
